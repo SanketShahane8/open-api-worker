aef955629c77ba1515fb2bf6625ba3d5cb1eb8bc
SanketShahane8 <sanket.shahane@outlook.com>
2025-03-18 22:40:51 +0530

    added code to call OpenAI API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import OpenAI from 'openai';
+
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -10,6 +12,43 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const openai = new OpenAI({ 
+			apiKey: env.OPENAI_API_KEY 
+		});
+		
+		try {
+
+			// const chatCompletion = await openai.chat.completions.create({
+			// 	model: "gpt-3.5-turbo",
+			// 	messages: [{ role: "user", content: "Should I trust stock predictions from Dodgy Dave?" }],
+			// 	temperature: 1.1,
+			// 	presence_penalty: 0,
+			// 	frequency_penalty: 0
+			// });
+
+			// const response = chatCompletion.choices[0].message.content;
+			// return new Response(JSON.stringify(response), {
+			// 	headers: { 'content-type': 'application/json' },
+			// });
+
+			const url = "https://openai-api-worker.guil-9d2.workers.dev";
+
+			const response = await this.fetch(url, {
+				method: "POST",
+				headers: {
+					"content-type": "application/json",
+				},
+				body: ''
+			});
+
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.error(`Error: ${error.message}`);
+			return new Response(JSON.stringify({ error: error.message }), {
+				status: 500,
+				headers: { 'content-type': 'application/json' },
+			});
+		}
 	},
 };
